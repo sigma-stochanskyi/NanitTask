410bb12078adf5862aa15f3119c5166dc3aa29ac
Marian Stochanskyi <marian.stochanskyi@sigma.software>
2022-06-09 18:15:42 +0300

    Setup navigation

---
+++ presentation/src/main/java/com/stochanskyi/nanittask/presentation/ui/features/birthday/BirthdayFragment.kt
@@ -0,0 +1,6 @@
+package com.stochanskyi.nanittask.presentation.ui.features.birthday
+
+import androidx.fragment.app.Fragment
+import com.stochanskyi.nanittask.presentation.R
+
+class BirthdayFragment : Fragment(R.layout.fragment_birthday)
---
+++ presentation/src/main/java/com/stochanskyi/nanittask/presentation/ui/features/profile/ProfileFragment.kt
@@ -0,0 +1,6 @@
+package com.stochanskyi.nanittask.presentation.ui.features.profile
+
+import androidx.fragment.app.Fragment
+import com.stochanskyi.nanittask.presentation.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile)
