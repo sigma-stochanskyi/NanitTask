0e87d21b28413fe42e4d4bf28c0b6ca9bc17be0f
Marian Stochanskyi <marian.stochanskyi@sigma.software>
2022-06-10 17:59:03 +0300

    Add data storage

--- data/src/main/java/com/stochanskyi/nanittask/data/di/DataModule.kt
+++ data/src/main/java/com/stochanskyi/nanittask/data/di/DataModule.kt
@@ -4,12 +4,17 @@ import com.stochanskyi.nanittask.androidcore.data.files.AppFileProvider
 import com.stochanskyi.nanittask.androidcore.data.files.AppFileProviderImpl
 import com.stochanskyi.nanittask.androidcore.data.textformatter.TextFormatter
 import com.stochanskyi.nanittask.androidcore.data.textformatter.TextFormatterImpl
+import com.stochanskyi.nanittask.data.storage.ProfileDataStorage
+import com.stochanskyi.nanittask.data.storage.ProfileDataStorageImpl
 import org.koin.dsl.module
+import java.net.DatagramSocketImpl
 
 val DataModule = module {
 
     factory<TextFormatter> { TextFormatterImpl() }
 
     factory<AppFileProvider> { AppFileProviderImpl(get()) }
+
+    single<ProfileDataStorage> { ProfileDataStorageImpl(get()) }
 
 }
---
+++ data/src/main/java/com/stochanskyi/nanittask/data/storage/ProfileDataStorage.kt
@@ -0,0 +1,60 @@
+package com.stochanskyi.nanittask.data.storage
+
+import android.content.Context
+import androidx.core.content.edit
+import java.util.prefs.Preferences
+
+interface ProfileDataStorage {
+    fun setProfileName(name: String)
+    fun setBirthday(date: String)
+    fun setProfileImageUri(image: String)
+
+    fun getProfileName(): String?
+    fun getBirthday(): String?
+    fun getProfileImageUri(): String?
+}
+
+class ProfileDataStorageImpl(
+    context: Context
+) : ProfileDataStorage {
+
+    companion object {
+        private const val PREFERENCES_NAME = "profile_storage"
+
+        private const val PROFILE_NAME_KEY = "profile_name"
+        private const val BIRTHDAY_KEY = "birthday"
+        private const val PROFILE_IMAGE_KEY = "profile_image"
+    }
+
+    private val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+
+    override fun setProfileName(name: String) {
+        prefs?.edit {
+            putString(PROFILE_NAME_KEY, name)
+        }
+    }
+
+    override fun setBirthday(date: String) {
+        prefs?.edit {
+            putString(BIRTHDAY_KEY, date)
+        }
+    }
+
+    override fun setProfileImageUri(image: String) {
+        prefs?.edit {
+            putString(PROFILE_IMAGE_KEY, image)
+        }
+    }
+
+    override fun getProfileName(): String? {
+        return prefs.getString(PROFILE_NAME_KEY, null)
+    }
+
+    override fun getBirthday(): String? {
+        return prefs.getString(BIRTHDAY_KEY, null)
+    }
+
+    override fun getProfileImageUri(): String? {
+        return prefs.getString(PROFILE_IMAGE_KEY, null)
+    }
+}
